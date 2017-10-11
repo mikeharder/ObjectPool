d08e4747ea31406cdfb3d1815f0184fcdc08ee3c
Mike Harder <mharder@microsoft.com>
2017-10-11 12:23:38 -0700

    Finish ObjectPool and add test app

--- ObjectPool/ObjectPool.cs
+++ ObjectPool/ObjectPool.cs
@@ -1,34 +1,70 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Threading;
 
 namespace ObjectPool
 {
     public class ObjectPool<T>
     {
-        private readonly ConcurrentBag<T> _objects;
+        private readonly ConcurrentBag<T> _availableObjects;
+        private readonly ConcurrentDictionary<T, object> _leasedObjects;
         private readonly Func<T> _objectGenerator;
+
+        public int AvailableObjectsCount
+        {
+            get
+            {
+                return _availableObjects.Count;
+            }
+        }
+
+        public int LeasedObjectsCount
+        {
+            get
+            {
+                return _leasedObjects.Count;
+            }
+        }
 
         public ObjectPool(Func<T> objectGenerator)
         {
-            _objects = new ConcurrentBag<T>();
             _objectGenerator = objectGenerator;
+
+            _availableObjects = new ConcurrentBag<T>();
+            _leasedObjects = new ConcurrentDictionary<T, object>();
         }
 
-        public T GetObject()
+        public T GetObject(object owner)
         {
-            if (_objects.TryTake(out var item))
+            T item;
+            if (!_availableObjects.TryTake(out item))
             {
-                return item;
+                item = _objectGenerator();
+            }
+
+            if (!_leasedObjects.TryAdd(item, owner))
+            {
+                Console.WriteLine("Failed add");
+            }
+
+            return item;
+        }
+
+        public bool PutObject(T item, object owner)
+        {
+            if (_leasedObjects.TryGetValue(item, out var leasedOwner) &&
+                Object.Equals(leasedOwner, owner))
+            {
+                while (!_leasedObjects.TryRemove(item, out var leasedOwner2)) { }
             }
             else
             {
-                return _objectGenerator();
+                // Object is not leased, or leased by a different owner
+                return false;
             }
-        }
 
-        public void PutObject(T item)
-        {
-            _objects.Add(item);
+            _availableObjects.Add(item);
+            return true;
         }
     }
 }
--- ObjectPool/Program.cs
+++ ObjectPool/Program.cs
@@ -1,50 +1,116 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Util;
 
 namespace ObjectPool
 {
     class Program
     {
-        private const int _concurrentTasks = 256;
-        private static int _iterations;
+        private const int _concurrentTasks = 128;
+
+        private static readonly ConcurrentRandom _random = new ConcurrentRandom();
+
+        private static readonly ConcurrentDictionary<string, string> _status = new ConcurrentDictionary<string, string>();
 
         static void Main(string[] args)
         {
             Console.WriteLine($"Concurrent Tasks: {_concurrentTasks}{Environment.NewLine}");
 
-            var pool = new ObjectPool<MyObject>(() => new MyObject());
-
             var printStatusTask = PrintStatus();
 
-            var testTasks = new Task[_concurrentTasks];
-            for (var i= 0; i < _concurrentTasks; i++)
+            var testTasks = new List<Task>
             {
-                testTasks[i] = Test(pool);
-            }
-            Task.WaitAll(testTasks);
+                Test("Guid\t\t", () => Guid.NewGuid()),
+                Test("ExecutionContext", () => Thread.CurrentThread.ExecutionContext)
+            };
+            Task.WaitAll(testTasks.ToArray());
 
             printStatusTask.Wait();
         }
 
         private static async Task PrintStatus()
         {
+            var top = Console.CursorTop;
             while (true)
             {
-                Console.Write($"\rIterations: {_iterations.ToString("N0")}");
+                Console.SetCursorPosition(0, top);
+
+                foreach (var status in _status.Values.OrderBy(s => s))
+                {
+                    Console.WriteLine(status);
+                }
+
                 await Task.Delay(TimeSpan.FromSeconds(1));
             }
         }
 
-        private static async Task Test(ObjectPool<MyObject> pool)
+        private static async Task Test(string name, Func<object> generateOwner)
+        {
+            var iterations = 0;
+            var failures = 0;
+
+            var pool = new ObjectPool<MyObject>(() => new MyObject());
+
+            var updateStatusTask = UpdateStatus(name, pool, () => iterations, () => failures);
+
+            var testTasks = new Task[_concurrentTasks];
+            for (var i = 0; i < _concurrentTasks; i++)
+            {
+                testTasks[i] = Test(pool, generateOwner,
+                    () => Interlocked.Increment(ref iterations), () => Interlocked.Increment(ref failures));
+            }
+            await Task.WhenAll(testTasks);
+
+            await updateStatusTask;
+        }
+
+        private static async Task UpdateStatus(string name, ObjectPool<MyObject> pool,
+            Func<int> iterations, Func<int> failures)
         {
             while (true)
             {
-                var obj = pool.GetObject();
+                var status = $"{name}\t" +
+                    $"Available Objects: {pool.AvailableObjectsCount}" +
+                    $", Leased Objects: {pool.LeasedObjectsCount}" +
+                    $", Iterations: {iterations().ToString("N0")}" +
+                    $", Failures: {failures().ToString("N0")}";
+
+                _status.AddOrUpdate(name, status, (n, s) => status);
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private static async Task Test(ObjectPool<MyObject> pool, Func<object> generateOwner,
+            Action incrementIterations, Action incrementFailures)
+        {
+            var owner = generateOwner();
+
+            while (true)
+            {
+                var rand = _random.Next();
+                var obj = pool.GetObject(owner);
+
+                obj.Value = rand;
+
                 await Task.Yield();
-                pool.PutObject(obj);
-                Interlocked.Increment(ref _iterations);
+
+                if (obj.Value != rand)
+                {
+                    incrementFailures();
+                }
+
+                pool.PutObject(obj, owner);
+
+                await Task.Yield();
+
+                pool.PutObject(obj, owner);
+
+                incrementIterations();
             }
         }
 
