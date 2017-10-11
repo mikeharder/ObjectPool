1f75c788e42256bacf7134dcfb817543664f5d9f
Mike Harder <mharder@microsoft.com>
2017-10-11 10:11:05 -0700

    Basic ObjectPool and test app

---
+++ ObjectPool/ObjectPool.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace ObjectPool
+{
+    public class ObjectPool<T>
+    {
+        private readonly ConcurrentBag<T> _objects;
+        private readonly Func<T> _objectGenerator;
+
+        public ObjectPool(Func<T> objectGenerator)
+        {
+            _objects = new ConcurrentBag<T>();
+            _objectGenerator = objectGenerator;
+        }
+
+        public T GetObject()
+        {
+            if (_objects.TryTake(out var item))
+            {
+                return item;
+            }
+            else
+            {
+                return _objectGenerator();
+            }
+        }
+
+        public void PutObject(T item)
+        {
+            _objects.Add(item);
+        }
+    }
+}
--- ObjectPool/Program.cs
+++ ObjectPool/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ObjectPool
 {
     class Program
     {
+        private const int _concurrentTasks = 256;
+        private static int _iterations;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Concurrent Tasks: {_concurrentTasks}{Environment.NewLine}");
+
+            var pool = new ObjectPool<MyObject>(() => new MyObject());
+
+            var printStatusTask = PrintStatus();
+
+            var testTasks = new Task[_concurrentTasks];
+            for (var i= 0; i < _concurrentTasks; i++)
+            {
+                testTasks[i] = Test(pool);
+            }
+            Task.WaitAll(testTasks);
+
+            printStatusTask.Wait();
+        }
+
+        private static async Task PrintStatus()
+        {
+            while (true)
+            {
+                Console.Write($"\rIterations: {_iterations.ToString("N0")}");
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private static async Task Test(ObjectPool<MyObject> pool)
+        {
+            while (true)
+            {
+                var obj = pool.GetObject();
+                await Task.Yield();
+                pool.PutObject(obj);
+                Interlocked.Increment(ref _iterations);
+            }
+        }
+
+        private class MyObject
+        {
+            public int Value { get; set; }
         }
     }
 }
